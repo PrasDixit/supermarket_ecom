c39eff8a5a4afd41e0171787ed05b44881424003
Prashanta D Acharya <apras.dixit@gmail.com>
2022-04-03 21:57:43 +0545

    Updated Home Page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,12 +3,11 @@ import BannerBotm from "../../Component/Home/BannerBotm";
 import Carousel from "../../Component/Home/Carousel";
 import BrandList from "../../Component/Home/BrandList";
 import NewOffers from "../../Component/Home/NewOffers";
-import OffersMain from "../../Component/Offers/OffersMain";
+
 const Home = () => {
   return (
     <div>
       <Slider />
-      <OffersMain />
       <BannerBotm />
       <Carousel />
       <BrandList />
