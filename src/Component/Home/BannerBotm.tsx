890b2cdf966cca589aa3a8fdf0f38d784db5fc7e
Prashanta D Acharya <apras.dixit@gmail.com>
2022-04-04 00:11:23 +0545

    Updated Banner(HomePage)

--- src/Component/Home/BannerBotm.tsx
+++ src/Component/Home/BannerBotm.tsx
@@ -1,4 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "../../Css/Banner.css";
+
 const BannerBotm = () => {
+  const baseURL = "https://uat.ordering-dalle.ekbana.net/";
+  const apiKey = "q0eq7VRCxJBEW6n1EJkHy4qNLgaS86ztm8DYhGMqerV1eldXa6";
+  const warehouseId = "1";
+
+  const [bannerBotm, setBannerBotm] = useState<any>();
+
+  useEffect(() => {
+    getBannerBotm();
+  }, []);
+
+  const getBannerBotm = async () => {
+    try {
+      const config = {
+        headers: { "Api-Key": apiKey, "Warehouse-Id": warehouseId },
+      };
+
+      const response = await axios.get(`${baseURL}api/v4/config`, config);
+      setBannerBotm(response.data.data.userGroup);
+      console.log(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="ban-bottom-w3l">
@@ -6,38 +33,35 @@ const BannerBotm = () => {
           <div className="col-md-6 ban-bottom3">
             <div className="ban-top">
               <img
-                src={"../../../assets/images/p2.jpg"}
-                className="img-responsive"
+                src={bannerBotm[0].image}
+                className="img-responsive banner-img"
                 alt=""
               />
-            </div>
-            <div className="ban-img">
-              <div className=" ban-bottom1">
-                <div className="ban-top">
-                  <img
-                    src={"../../../assets/images/p3.jpg"}
-                    className="img-responsive"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="ban-bottom2">
-                <div className="ban-top">
-                  <img
-                    src={"../../../assets/images/p4.jpg"}
-                    className="img-responsive"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="clearfix"></div>
             </div>
           </div>
           <div className="col-md-6 ban-bottom">
             <div className="ban-top">
               <img
-                src={"../../../assets/images/111.jpg"}
-                className="img-responsive"
+                src={bannerBotm[4].image}
+                className="img-responsive banner-img"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="col-md-6 ban-bottom">
+            <div className="ban-top">
+              <img
+                src={bannerBotm[2].image}
+                className="img-responsive banner-img"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="col-md-6 ban-bottom">
+            <div className="ban-top">
+              <img
+                src={bannerBotm[1].image}
+                className="img-responsive banner-img"
                 alt=""
               />
             </div>
