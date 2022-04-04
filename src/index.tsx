78a7d5fd6fc531f7d7e4ed066e4353e46d834508
Prashanta D Acharya <apras.dixit@gmail.com>
2022-04-04 11:45:43 +0545

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
 import "react-slideshow-image/dist/styles.css";
 import reportWebVitals from "./reportWebVitals";
 
